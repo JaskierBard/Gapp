2597d8e480477ca208f3dfcfa91b407b4fdcccde
JaskierBared <mateuszswiderski009@gmail.com>
2024-01-24 15:47:59 +0100

    Added new font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,42 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
 
 export default function App() {
+  const [isAppReady, setIsAppReady] = useState(false);
+
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      "gothic-font": require("./assets/fonts/gothic_font.ttf"),
+    });
+  };
+
+  useEffect(() => {
+    const prepareApp = async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await loadFonts();
+        // Dodaj inne operacje inicjalizacyjne, jeśli są potrzebne
+
+        setIsAppReady(true);
+      } catch (error) {
+        console.warn("Error while preparing the app:", error);
+      } finally {
+        await SplashScreen.hideAsync();
+      }
+    };
+
+    prepareApp();
+  }, []);
+
+  if (!isAppReady) {
+    return <Text>Loading...</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Gapp</Text>
+      <Text style={styles.text}>Gapp</Text>
     </View>
   );
 }
@@ -15,5 +47,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontFamily: "gothic-font",
+    fontSize: 22,
   },
 });
